d1c2d78d67058f4b6c8dd0fa8e59d6b201faf968
franck <carnet.franck.paul@gmail.com>
2025-06-03 15:38:38 +0200

    New release 6.1

--- _define.php
+++ _define.php
@@ -15,9 +15,9 @@ $this->registerModule(
     'Last Spams Dashboard Module',
     'Display last spams on dashboard',
     'Franck Paul',
-    '6.0',
+    '6.1',
     [
-        'date'        => '2025-05-04T10:21:42+0200',
+        'date'        => '2025-06-03T15:38:38+0200',
         'requires'    => [['core', '2.34']],
         'permissions' => 'My',
         'type'        => 'plugin',
